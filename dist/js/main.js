965bd2e44fffe3583c2ae83d88d9fd7b5affc102
Nemanja Vukovic <nemanja16vukovic@gmail.com>
2022-06-14 01:20:04 +0200

    navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,18 @@
-console.log('test');
+function menu () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navigation = document.querySelector('.navbar');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if(!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+            navigation.classList.add('open');
+        }else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+            navigation.classList.remove('open');
+        }
+    });
+}
+
+menu();
